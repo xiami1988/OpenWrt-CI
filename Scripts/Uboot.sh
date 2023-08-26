af5d691ee2ba17bc18475b3c1774bcab226b96ee
abandon-xyz <1484451453@qq.com>
2023-08-26 17:19:36 +0800

    uboot: add imou_lc-hx3001 clt_r30b1

--- Scripts/Uboot.sh
+++ Scripts/Uboot.sh
@@ -1,4 +1,4 @@
-#360 T7 cetron_ct3003  jcg_q30 konka_komi-a31 wr30u cmcc_rax3000m redmi_ax6000
+#360 T7 cetron_ct3003  jcg_q30 konka_komi-a31 wr30u cmcc_rax3000m imou_lc-hx3001 clt_r30b1 redmi_ax6000
 SOC=mt7981 BOARD=360t7 ./build.sh
 SOC=mt7981 BOARD=cetron_ct3003 ./build.sh
 SOC=mt7981 BOARD=jcg_q30 ./build.sh
@@ -6,5 +6,8 @@ SOC=mt7981 BOARD=konka_komi-a31 ./build.sh
 SOC=mt7981 BOARD=wr30u ./build.sh
 SOC=mt7981 BOARD=wr30u MULTI_LAYOUT=1 ./build.sh
 SOC=mt7981 BOARD=cmcc_rax3000m ./build.sh
+SOC=mt7981 BOARD=imou_lc-hx3001 ./build.sh
+SOC=mt7981 BOARD=clt_r30b1 ./build.sh
+SOC=mt7981 BOARD=clt_r30b1 MULTI_LAYOUT=1 ./build.sh
 SOC=mt7986 BOARD=redmi_ax6000 ./build.sh
 SOC=mt7986 BOARD=redmi_ax6000 MULTI_LAYOUT=1 ./build.sh
